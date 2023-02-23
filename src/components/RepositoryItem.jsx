81e19879855ccf4032388e00b3b5a92949106963
Heikki Mantere <heikki.mantere@gmail.com>
2023-02-23 16:52:49 +0200

    10.3

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,38 @@
+import Constants from "expo-constants";
+import { Text, StyleSheet, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+    flexShrink: 1,
+  },
+});
+
+const RepositoryItem = ({item}) => {
+  return (
+    <View style={styles.container}>
+      <Text>Full name: {item.fullName}</Text>
+      <Text>Description: {item.description}</Text>
+      <Text>Language: {item.language}</Text>
+      <Text>Stars: {item.stargazersCount}</Text>
+      <Text>Forks: {item.forksCount}</Text>
+      <Text>Reviews: {item.reviewCount}</Text>
+      <Text>Rating: {item.ratingAverage}</Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
+
+/**
+ *  id: 'jaredpalmer.formik',
+    fullName: 'jaredpalmer/formik',
+    description: 'Build forms in React, without the tears',
+    language: 'TypeScript',
+    forksCount: 1589,
+    stargazersCount: 21553,
+    ratingAverage: 88,
+    reviewCount: 4,
+    ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/4060187?v=4',
+ */
