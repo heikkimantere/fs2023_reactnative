a92fc07d4de6958dfb3e230f223533c4c4c56159
Heikki Mantere <heikki.mantere@gmail.com>
2023-02-27 12:51:53 +0200

    10.16

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,8 +1,11 @@
-import { StyleSheet, ScrollView, View } from "react-native";
+import { StyleSheet, ScrollView, View, Pressable } from "react-native";
 import Constants from "expo-constants";
 import Text from "./Text";
 import theme from "../theme";
 import { Link } from "react-router-native";
+import { useApolloClient, useQuery } from "@apollo/client";
+import { ME } from "../graphql/queries";
+import { useAuthStorage } from "../hooks/useAuthStorage";
 
 const styles = StyleSheet.create({
   container: {
@@ -11,16 +14,26 @@ const styles = StyleSheet.create({
   navBar: {
     paddingTop: Constants.statusBarHeight,
     flexDirection: "row",
+    justifyContent: "flex-start",
   },
   link: {
     padding: 15,
-    flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    whiteSpace: "nowrap",
   },
 });
 
 const AppBar = () => {
+  const { data } = useQuery(ME);
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
+  const me = data?.me;
+  const signOut = async () => {
+    await authStorage.removeAccessToken();
+    apolloClient.resetStore();
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.navBar} horizontal={true}>
@@ -29,11 +42,19 @@ const AppBar = () => {
             Repositories
           </Text>
         </Link>
-        <Link style={styles.link} to="/signin">
-          <Text color="white" fontWeight="bold">
-            Sign in
-          </Text>
-        </Link>
+        {me?.id ? (
+          <Pressable style={styles.link} onPress={signOut}>
+            <Text color="white" fontWeight="bold" numberOfLines={1}>
+              Sign out {me.username}
+            </Text>
+          </Pressable>
+        ) : (
+          <Link style={styles.link} to="/signin">
+            <Text color="white" fontWeight="bold">
+              Sign in
+            </Text>
+          </Link>
+        )}
       </ScrollView>
     </View>
   );
